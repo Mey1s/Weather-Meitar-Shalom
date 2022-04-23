883fedc8b915efe1d0255daec9908b9dbafd92c3
m <meytar733@gmail.com>
2022-04-23 15:37:46 +0300

    master

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import Footer from './footer/footer';
-import Header from './header/header';
+import React from "react";
+import { useSelector } from "react-redux";
+import { WeatherAppState } from "../redux/weather";
+import Footer from "./footer/footer";
+import Header from "./header/header";
 
 import "./layout.scss";
 
 interface LayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = (props) => {
-
-    return(
-        <>
-        <Header />
-        {props.children}
-        <Footer />
-        </>
-    )
-
-}
+  const darkMode = useSelector((state: WeatherAppState) => state.darkMode);
+  return (
+    <div className="layoutContainer" data-theme={darkMode ? "dark" : "light"}>
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
-
